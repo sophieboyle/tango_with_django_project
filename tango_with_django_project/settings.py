3c9be97b8755a6a0aadd3758c20dab931e652e06
sophieboyle <sophieboyle4028@gmail.com>
2020-01-18 03:20:00 +0000

    Added image

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Template Directory
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+# Static Files Directory
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -121,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
