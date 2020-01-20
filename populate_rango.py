957e168fbeddea1cae9ac3d6c5c4ee2052f48126
sophieboyle <sophieboyle4028@gmail.com>
2020-01-20 01:48:38 +0000

    Changed views and likes for each category in population script

--- populate_rango.py
+++ populate_rango.py
@@ -37,13 +37,13 @@ def populate():
     ]
 
     # Categories, each mapped to their respective list of pages
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
+            'Django': {'pages': django_pages, 'views':64, 'likes':32},
+            'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16}}
 
     # For each category, adds the category and its respective pages
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -63,9 +63,11 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     # Initialising category
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     # Add category to db
     c.save()
     return c
