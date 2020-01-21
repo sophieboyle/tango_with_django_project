eb14d7b3d2d43a75d0ed319035fd313f5ce1c582
sophieboyle <sophieboyle4028@gmail.com>
2020-01-21 02:17:34 +0000

    Added category viewing pages. New view category_show(), and url path to each category

--- rango/views.py
+++ rango/views.py
@@ -14,3 +14,16 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Sophie'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        # Returns category if exists, otherwise, jumps to except
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    return render(request, 'rango/category.html', context=context_dict)
