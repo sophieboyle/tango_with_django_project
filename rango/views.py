a8a92689f7b3483908a74c3fa44f085ed6c16bb3
sophieboyle <sophieboyle4028@gmail.com>
2020-01-21 02:26:22 +0000

    Fixed error for categories which don't exist

--- rango/views.py
+++ rango/views.py
@@ -24,6 +24,6 @@ def show_category(request, category_name_slug):
         context_dict['category'] = category
         context_dict['pages'] = pages
     except Category.DoesNotExist:
-        context_dict['category'] = category
-        context_dict['pages'] = pages
+        context_dict['category'] = None
+        context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
